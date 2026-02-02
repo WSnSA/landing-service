a29bb2f2cf5adbd13336284622e6e3db6ec87d05
boyaruska <60504713+bayrtsogt@users.noreply.github.com>
2026-02-02 11:47:37 +0800

    git add . git commit -m "init: spring boot backend skeleton"

---
+++ src/test/java/mn/landing/landing_service/LandingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.landing.landing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
