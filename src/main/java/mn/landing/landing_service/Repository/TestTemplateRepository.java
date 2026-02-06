71c66bc52eec79211fb5a169a803bd46fbe84df7
boyaruska <60504713+bayrtsogt@users.noreply.github.com>
2026-02-06 09:15:17 +0800

    #deploy

--- src/main/java/mn/landing/landing_service/Repository/TestTemplateRepository.java
+++
@@ -1,8 +0,0 @@
-package mn.landing.landing_service.Repository;
-
-import mn.landing.landing_service.Entity.TestTemplate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestTemplateRepository
-        extends JpaRepository<TestTemplate, Long> {
-}
